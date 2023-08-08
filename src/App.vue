9f630d0666256c4c8e60d1303d09f79f2dbabb29
talhatonmoy <talha.tonmoy.ck@gmail.com>
2023-08-08 13:05:57 +0600

    user authentication

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, reactive, computed } from 'vue'
+import LoginRegister from './components/LoginRegister.vue';
+
+
+</script>
+
+<template>
+  <section class="flex h-screen w-full">
+  <div class="w-1/2"
+        style="background-image: url(https://images.unsplash.com/photo-1690555575753-7aa27df96b52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80); background-repeat: no-repeat; background-size: cover;">
+        <h1 class="mb-5 text-2xl mt-10 ml-10 text-white">Kosmos!</h1>
+      </div>
+      <div class="w-1/2 flex flex-col justify-center items-center bg-gray-200">
+        <h2 class="mb-5 text-xl">Login or register</h2>
+        <div class="w-full max-w-xs">
+          <!-- Form -->
+          <LoginRegister/>
+          <!-- Form End -->
+          <p class="text-center text-gray-500 text-xs">
+            &copy;2020 Acme Corp. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
---
+++ src/components/FormSwitchButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+const isLogin = ref(true)
+
+function changeForm() {
+    isLogin.value = !isLogin.value;
+    console.log(isLogin.value)
+}
+</script>
+
+<template>
+   
+</template>
+
+<style scoped>
+</style>
